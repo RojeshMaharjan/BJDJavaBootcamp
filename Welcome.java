1397cd1247846f4b5dcc07488cd8ff73374cfab3
Rojesh Maharjan <roj2mah@gmail.com>
2023-04-20 14:56:53 -0500

    First Committing all the files to the repositories

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+
+public class Welcome {
+	//Execution will start from main method
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//to print the information on consolde we can use
+		System.out.println("Welcome to takeo bootcamp");
+	}
+
+}
